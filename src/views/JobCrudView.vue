ce8a5da745d92b4428c4f611173214cb82e18eec
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-04-27 12:37:02 +0200

    Mockup crud job

--- src/views/JobCrudView.vue
+++ src/views/JobCrudView.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
-    <h1>Jobs CRUD View</h1>
+    <v-container>
+      <v-row>
+        <v-col>
+          <main-card>
+            <template #title>Create a Job Offer:</template>
+            <template #content>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    dense
+                    x-small
+                    background-color="white"
+                    outlined
+                    placeholder="Job Title"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-textarea
+                    outlined
+                    name=""
+                    placeholder="Job description"
+                    value=""
+                    background-color="white"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </template>
+            <template #actions>
+              <v-btn class="primary--text">Save</v-btn>
+            </template>
+          </main-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
