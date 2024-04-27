9c7a45a5671c334a59707863b2190c160e8ed4b9
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-04-27 10:46:49 +0200

    MainComponent to use in all parts of application

---
+++ src/components/MainCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="primary" elevation="2">
+    <v-card-title>
+      <slot name="title"></slot>
+    </v-card-title>
+
+    <v-card-subtitle>
+      <slot name="subtitle"></slot>
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <slot name="actions"></slot>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
