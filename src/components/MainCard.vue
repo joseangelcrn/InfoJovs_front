e1b7bc1121a16bb1001089c8bf40a481a91078c5
joseangelcrn <jose.angel.cabeza.crn@gmail.com>
2024-04-28 14:02:41 +0200

    customization improvement of main card component

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,26 +1,53 @@
 <template>
-  <v-card class="primary" elevation="2">
-    <v-card-title class="white--text">
+  <v-card :class="customClassVcard ?? 'primary'" elevation="2">
+    <v-card-title  v-if="$slots.title" :class="customClassTitle ?? 'white--text'" >
       <slot name="title"></slot>
     </v-card-title>
 
-    <v-card-subtitle>
+    <v-card-subtitle  v-if="$slots.subtitle"  :class="customClassSubTitle ?? ''">
       <slot name="subtitle"></slot>
     </v-card-subtitle>
 
-    <v-card-text>
-        <slot name="content"></slot>
+    <v-card-text v-if="$slots.content"  :class="customClassContent ?? ''">
+      <slot name="content"></slot>
     </v-card-text>
 
-    <v-card-actions>
+    <v-card-actions v-if="$slots.actions"  :class="customClassActions ?? ''">
       <slot name="actions"></slot>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    customClassVcard: {
+      type: String,
+      default: null,
+      required: false,
+    },
+    customClassTitle: {
+      type: String,
+      default: null,
+      required: false,
+    },
+    customClassSubTitle: {
+      type: String,
+      default: null,
+      required: false,
+    },
+    customClassContent: {
+      type: String,
+      default: null,
+      required: false,
+    },
+    customClassActions: {
+      type: String,
+      default: null,
+      required: false,
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
