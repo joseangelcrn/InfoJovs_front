44ec801ef73b0fd7a63baa81d63d98839efbd086
joseangelcrn <jose.angel.cabeza.crn@gmail.com>
2024-04-28 15:26:08 +0200

    JobPreview component : to represent most important information about the job .

---
+++ src/components/JobPreview.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list-item three-line class="white rounded mt-2">
+    <v-list-item-content>
+      <v-list-item-title>{{ title }}</v-list-item-title>
+      <v-list-item-subtitle  :class="(statusColor ?? 'black') + '--text'">{{
+        statusText
+      }}</v-list-item-subtitle>
+      <v-list-item-subtitle>{{ description }}</v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-icon class="mt-6">
+      <v-btn icon class="align-center"> <v-icon>mdi-eye</v-icon> </v-btn>
+    </v-list-item-icon>
+  </v-list-item>
+</template>
+<script>
+export default {
+  name: "job-preview",
+  props: ["title", "description", "statusText", "statusColor"],
+
+  data() {
+    return {};
+  },
+};
+</script>
