4e24b79ea73b9aa4e8f170aaaaf80f2671b7ae03
joseangelcrn <jose.angel.cabeza.crn@gmail.com>
2024-04-28 13:38:03 +0200

    Fix middleware  router when access to root path  ( / ) being auth..ed and unauth..ed
    Formatting file

--- src/router/index.js
+++ src/router/index.js
@@ -1,70 +1,68 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import HomeView from '@/views/HomeView.vue'
-import NotFound from '@/views/NotFound.vue'
-import JobView from '@/views/JobView.vue'
-import JobCrudView from '@/views/JobCrudView.vue'
-import MyJobsView from '@/views/MyJobsView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import HomeView from "@/views/HomeView.vue";
+import NotFound from "@/views/NotFound.vue";
+import JobView from "@/views/JobView.vue";
+import JobCrudView from "@/views/JobCrudView.vue";
+import MyJobsView from "@/views/MyJobsView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
-    path: '/home',
-    name: 'home',
-    component: HomeView
+    path: "/home",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/jobs',
-    name: 'jobs',
-    component: JobView
+    path: "/jobs",
+    name: "jobs",
+    component: JobView,
   },
   {
-    path: '/offer_job',
-    name: 'offerJob',
-    component: JobCrudView
+    path: "/offer_job",
+    name: "offerJob",
+    component: JobCrudView,
   },
   {
-    path: '/my_jobs',
-    name: 'myJobs',
-    component: MyJobsView
+    path: "/my_jobs",
+    name: "myJobs",
+    component: MyJobsView,
   },
   {
-    path: '*',
-    name: 'notFound',
-    component: NotFound
-  }
-]
+    path: "*",
+    name: "notFound",
+    component: NotFound,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
+router.beforeEach(async (to, from, next) => {
+  console.log("middleware");
+  let user = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null;
+  console.log("user = ", user);
+  console.log("to = ", to);
 
-router.beforeEach(async (to, from,next) => {
-  console.log('middleware');
-  let user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null ;
-  console.log('user = ',user);
-  console.log('to = ', to)
-
-  if (to.name == 'login' && user){
-    next({name:'home'})
+  if ((to.name == "login" || to.path == "/") && user) {
+    next({ name: "home" });
+  } else if ((to.name == "home" || to.path == "/") && !user) {
+    next({ name: "login" });
   }
-  else if(to.name == 'home' && !user){
-    next({name:'login'}) 
-  }
+
   next();
+});
 
-  
-})
-
-
-export default router
+export default router;
