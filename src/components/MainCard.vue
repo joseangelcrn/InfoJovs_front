d676472f2eb041390a026917253513f1c10e4067
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-05-02 12:53:22 +0200

    Loader implemented in main card component

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,18 +1,30 @@
 <template>
   <v-card :class="customClassVcard ?? 'primary'" elevation="2">
-    <v-card-title  v-if="$slots.title" :class="customClassTitle ?? 'white--text'" >
+    <v-card-title
+      v-if="$slots.title"
+      :class="customClassTitle ?? 'white--text'"
+    >
       <slot name="title"></slot>
     </v-card-title>
 
-    <v-card-subtitle  v-if="$slots.subtitle"  :class="customClassSubTitle ?? ''">
+    <v-card-subtitle v-if="$slots.subtitle" :class="customClassSubTitle ?? ''">
       <slot name="subtitle"></slot>
     </v-card-subtitle>
 
-    <v-card-text v-if="$slots.content"  :class="customClassContent ?? ''">
+    <v-card-text>
+      <v-progress-circular
+        :size="80"
+        :width="9"
+        color="white"
+        indeterminate
+        v-show="loading"
+      ></v-progress-circular>
+    </v-card-text>
+    <v-card-text v-if="$slots.content" :class="customClassContent ?? ''">
       <slot name="content"></slot>
     </v-card-text>
 
-    <v-card-actions v-if="$slots.actions"  :class="customClassActions ?? ''">
+    <v-card-actions v-if="$slots.actions" :class="customClassActions ?? ''">
       <slot name="actions"></slot>
     </v-card-actions>
   </v-card>
@@ -44,6 +56,11 @@ export default {
     customClassActions: {
       type: String,
       default: null,
+      required: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
       required: false,
     },
   },
