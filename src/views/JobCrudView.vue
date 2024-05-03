beef33bfd23a18e91b70f95a190cad835e3d0578
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-05-03 15:40:21 +0200

    CRUD job form  finished (only creation)

--- src/views/JobCrudView.vue
+++ src/views/JobCrudView.vue
@@ -1,48 +1,89 @@
 <template>
   <div>
     <v-container>
-      <v-row>
-        <v-col>
+      <v-row d-flex justify="center">
+        <v-col cols="10">
           <main-card>
             <template #title>Create a Job Offer:</template>
             <template #content>
               <v-row>
-                <v-col cols="12">
+                <v-col>
                   <v-text-field
                     v-model="title"
+                    hint="Title which shortly describe job"
+                    solo
                     dense
                     x-small
                     background-color="white"
                     outlined
-                    placeholder="Job Title"
+                    placeholder="Full-Stack PHP & Vue"
                     :error="errors.title != null"
                     :error-messages="errors.title"
-                    >
-                  <template #message="{message}">
-                    <b class="white rounded-pill pa-3">{{message}}</b>
-                  </template>
+                  >
+                    <template #message="{ message }">
+                      <b class="white rounded-pill pa-3">{{ message }}</b>
+                    </template>
                   </v-text-field>
                 </v-col>
-                <v-col cols="12">
-                  <v-textarea
-                    v-model="description"
-                    outlined
-                    name=""
-                    placeholder="Job description"
-                    value=""
-                    background-color="white"
-                    :error="errors.description != null"
-                    :error-messages="errors.description"
+                <v-col>
+                  <v-combobox
+                    v-model="tags"
+                    :items="items"
+                    chips
+                    clearable
+                    hint="Tags which describes job"
+                    multiple
+                    solo
+                    dense
+                    @change="onChangeTags"
+                    :error="errors.tags != null"
+                    :error-messages="errors.tags"
                   >
-                  <template #message="{message}">
-                    <b class="white rounded-pill pa-3">{{message}}</b>
+                    <template
+                      v-slot:selection="{ attrs, item, select, selected }"
+                    >
+                      <v-chip
+                        color="primary"
+                        v-bind="attrs"
+                        :input-value="selected"
+                        close
+                        @click="select"
+                        @click:close="removeTag(item)"
+                      >
+                        <strong>{{ item }}</strong
+                        >&nbsp;
+                      </v-chip>
+                    </template>
+                    <template #message="{ message }">
+                      <b class="white rounded-pill pa-3">{{ message }}</b>
+                    </template>
+                  </v-combobox>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-textarea
+                  v-model="description"
+                  :hint="'Describe extensively information about job. Such as required experience , which technologies you need to be experienced,retributions..'"
+                  solo
+                  outlined
+                  placeholder="Job description"
+                  background-color="white"
+                  :error="errors.description != null"
+                  :error-messages="errors.description"
+                  
+                >
+                  <template #message="{ message }">
+                    <b class="white rounded-pill pa-3">{{ message }}</b>
                   </template>
                 </v-textarea>
                 </v-col>
               </v-row>
             </template>
             <template #actions>
-              <v-btn v-on:click="onSave" small class="primary--text">Save</v-btn>
+              <v-btn v-on:click="onSave" small class="primary--text"
+                >Save</v-btn
+              >
             </template>
           </main-card>
         </v-col>
@@ -57,27 +98,49 @@ export default {
     return {
       title: "",
       description: "",
+      tags:[],
+      items:[],
       errors: {
         title: null,
         description: null,
+        tags:null
       },
     };
   },
   methods: {
-    onSave: function  () {
+    onSave: async function () {
       this.errors = {
         title: null,
         description: null,
+        tags:null
       };
 
       if (this.title.trim().length === 0) {
         this.errors.title = "Title can not be empty.";
-      } else if (this.description.trim().length === 0) {
+      } 
+      else if(this.tags.length < 3){
+        this.errors.tags = "Please add, at least, 3 tags."
+      }
+      else if (this.description.trim().length === 0) {
         this.errors.description = "Description can not be empty";
-      } else {
+      }
+      else {
         console.log("save Job !");
+        let response = await this.$proxy.createJob({
+          title:this.title,
+          description:this.description,
+          tags:this.tags
+        });
+        console.log('response ',response);
       }
     },
+    onChangeTags: function (data) {
+      let newTag = this.tags[this.tags.length-1];
+    },
+    removeTag: function(item){
+      console.log('remove tag');
+      this.tags.splice(this.tags.indexOf(item), 1)
+    }
   },
 };
 </script>
