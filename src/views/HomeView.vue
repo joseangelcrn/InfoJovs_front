18414542948b36a2048e9b68e4d7cc5de4ad9839
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-06-05 17:39:40 +0200

    Home View Rework

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,62 +4,49 @@
       <v-col>
         <main-card>
           <template #content>
-            <v-list-item three-line>
-              <v-list-item-avatar
-                tile
-                size="100"
-                color="grey"
-                class="mr-10"
-              ></v-list-item-avatar>
-              <v-list-item-content class="white--text">
-                <div class="text-overline mb-4 white--text">
-                  {{ user.data?.name }}
+            <div class="d-flex">
+              <img src="@/assets/images/user_icon.png"
+                   style="background-color: white;border-radius: 30px; width: 100px; height: 100px;" alt="User Image">
+              <div class="ml-2">
+                <h1 class="white--text">{{ user.data.name }}</h1>
+                <h2 class="mt-2 white primary--text pa-1 rounded"> {{ user.data?.professional_profile.title }}</h2>
+                <div class="d-flex justify-space-between mt-2 white primary--text pa-1 rounded">
+                  <v-icon color="primary">mdi-cake-variant-outline</v-icon>
+                  <span class="text-h7 font-weight-bold"> {{ user.data?.birth_date }}</span>
                 </div>
-
-                <v-list-item-title class="text-h5 mb-1">
-                  {{ user.data?.professional_profile.title }}
-                </v-list-item-title>
-                <v-list-item-subtitle class="white--text">
-                  <v-chip color="orange" label>
-                    <b>Roles:&nbsp;&nbsp; </b>
-                    {{
-                      user.roles
+              </div>
+            </div>
+            <v-divider class="my-2"></v-divider>
+            <div class="d-flex">
+              <div class="d-flex justify-space-between mt-2 white primary--text pa-1 rounded">
+                <v-icon color="orange">mdi-key-variant</v-icon>
+                <span class="text-h7 font-weight-bold orange--text">    {{
+                    user.roles
                         .map((role) => $common.capitalizeFirstLetter(role))
                         .join(",")
-                    }}
-                  </v-chip>
-                </v-list-item-subtitle>
-              </v-list-item-content>
-<!--              <v-list-item-content>-->
-<!--                <tooltip text="Tooltip text" :primary="false">-->
-<!--                    <v-btn>Hola</v-btn>-->
-<!--                </tooltip>-->
-<!--              </v-list-item-content>-->
-            </v-list-item>
-            <v-list-item two-line>
-              <v-chip color="indigo white--text" label
-                ><b>Birth Date: </b>{{ user.data?.birth_date }}</v-chip
-              >
-            </v-list-item>
-            <v-divider class="my-2"></v-divider>
-            <v-list-item v-if="$common.hasRole('employee')">
-              <v-list-item-content>
-                <span class="white--text mb-4"
-                  >You have
-                  <b class="black--text">{{candidature.data.length}}</b>
-                  Job Applications</span
-                >
-                <v-btn
+                  }}</span>
+              </div>
+            </div>
+          </template>
+        </main-card>
+        <div class="my-6"></div>
+
+        <main-card class="mt-6" v-if="$common.hasRole('Employee')">
+          <template #content>
+            <div class="d-flex justify-space-between">
+              <v-btn
+                  class="white primary--text"
                   elevation="2"
                   max-width="150"
                   :to="{ name: 'myCandidatures' }"
-                >
-                  My Candidatures</v-btn
-                >
-              </v-list-item-content>
-            </v-list-item>
+              >
+                My Candidatures
+              </v-btn
+              >
+              <v-chip class="white primary--text  align-self-center"
+              >You have <v-chip small class="primary white--text font-weight-bold mx-2">{{ candidature.data.length }}</v-chip> Job Applications</v-chip>
+            </div>
           </template>
-          <template></template>
         </main-card>
       </v-col>
     </v-row>
@@ -67,7 +54,8 @@
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import {mapActions, mapState} from "vuex";
+
 export default {
   data() {
     return {
@@ -83,7 +71,7 @@ export default {
     ...mapState({
       user: "user",
       job: "job",
-      candidature:"candidature"
+      candidature: "candidature"
     }),
   },
   mounted: async function () {
