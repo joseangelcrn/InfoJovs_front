b0f52845e856f9747e6e87a85e3bc64d674975be
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-06-09 13:44:16 +0200

    Formatting cv -store

--- src/store/cv.js
+++ src/store/cv.js
@@ -15,7 +15,7 @@ const cv = {
         setData: function (state, data) {
             state.data = data;
         },
-        hideModal:function (state){
+        hideModal: function (state) {
             console.log('vuex- hide modal')
             state.modal.show = false;
         },
@@ -30,29 +30,28 @@ const cv = {
             }
         },
 
-
-        editExperience: function (state,experienceId){
-            let experience = state.data.experiences.find((item)=>{
+        editExperience: function (state, experienceId) {
+            let experience = state.data.experiences.find((item) => {
                 return item.id === experienceId
             });
-            console.log('exp id = '+ experienceId)
+            console.log('exp id = ' + experienceId)
             state.modal = {
                 show: true,
                 type: 'experience',
                 data: common.deepClone(experience),
-                aux:{
-                    start_date:null,
-                    finish_date:null
+                aux: {
+                    start_date: null,
+                    finish_date: null
                 },
-                menus:{
-                    start_date:false,
-                    finish_date:false,
+                menus: {
+                    start_date: false,
+                    finish_date: false,
                 },
             }
         },
 
-        editSkill: function (state,skillId){
-            let skill = state.data.skills.find((item)=>{
+        editSkill: function (state, skillId) {
+            let skill = state.data.skills.find((item) => {
                 return item.id === skillId
             });
 
@@ -63,50 +62,49 @@ const cv = {
             }
         },
 
-        refreshData: function (state){
+        refreshData: function (state) {
             console.log('refresh data ')
-            let {type,data} = state.modal;
+            let {type, data} = state.modal;
 
-            if (type === 'summary'){
+            if (type === 'summary') {
                 state.data.summary = data;
-            }
-            else{ //experience | skill
-                let key = type+'s'; // experience = experiences | skill = skills
-                state.data[key] = state.data[key].map((item)=>{
-                    if (item.id === data.id){
+            } else { //experience | skill
+                let key = type + 's'; // experience = experiences | skill = skills
+                state.data[key] = state.data[key].map((item) => {
+                    if (item.id === data.id) {
                         item = data;
                     }
                     return item;
                 })
             }
         },
-        pushData: function(state){
-            if (state.modal.type === 'summary'){
+        pushData: function (state) {
+            if (state.modal.type === 'summary') {
                 state.data.summary = state.modal.data;
-            }else{
-                let key = state.modal.type+'s'; // experience = experiences | skill = skills
+            } else {
+                let key = state.modal.type + 's'; // experience = experiences | skill = skills
                 state.data[key].push(state.modal.data);
             }
 
         },
 
-        updateStartDate: function(state,data){
+        updateStartDate: function (state, data) {
             state.modal.data.start_date = data;
         },
-        updateFinishDate: function(state,data){
+        updateFinishDate: function (state, data) {
             state.modal.data.finish_date = data;
         },
-        resetAuxVars:function (state){
+        resetAuxVars: function (state) {
             state.modal.aux = {
-                start_date:null,
-                finish_date:null
+                start_date: null,
+                finish_date: null
             }
         },
-        setAuxVar: function(state, {key,value}){
+        setAuxVar: function (state, {key, value}) {
             state.modal.aux[key] = value;
         },
-        delete: function(state,{id,type}){
-            state.data[type] = state.data[type].filter((item)=>{
+        delete: function (state, {id, type}) {
+            state.data[type] = state.data[type].filter((item) => {
                 return item.id !== id;
             })
         }
@@ -119,20 +117,19 @@ const cv = {
             console.log('response cv = ', response.data)
             commit('setData', response.data.cv);
         },
-        save: async function({commit,state}){
+        save: async function ({commit, state}) {
             let {data} = state.modal;
             let response = {};
-            if (data.id){
+            if (data.id) {
                 commit('refreshData');
-            }
-            else{
+            } else {
                 commit('pushData');
             }
             commit('resetAuxVars');
             commit('hideModal');
         },
-        delete: async function({commit},payload){
-            commit('delete',payload)
+        delete: async function ({commit}, payload) {
+            commit('delete', payload)
         }
     },
 };
